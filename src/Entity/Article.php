f5f224f015d7c1960d29ca095bd9005431446ca0
ChaCoutinot <charlotte.coutinot@hotmail.com>
2022-07-06 10:47:35 +0200

    Project Blog

--- src/Entity/Article.php
+++ src/Entity/Article.php
@@ -5,10 +5,18 @@ namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
 
+
+//on créer un ORM une classe (car route est pour les controllers) pour que l'annotation soit prise en compte et création d'une entité
+// ORM = mapping objet-relationnel ; outil qui va permettre de se mettre en interface entre le programme et la BDD
 /**
  * @ORM\Entity()
  */
+
+//on créer une classe
+
 class Article {
+
+    //on créer les annotations pour definir ce qu'il va etre créé dans la BDD
 
     /**
      * @ORM\Column(type="integer")
@@ -23,5 +31,10 @@ class Article {
      */
     public $title;
 
+
+    // Pour creer ta base de donnée dans le terminal
+    // php bin/console doctrine:database:create
+    // migration de fichier dans la BDD 
+    // php bin/console make:migration
 
 }
