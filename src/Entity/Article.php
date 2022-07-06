79753f25f6b346a9185493c8d3121d61543e62b7
ChaCoutinot <charlotte.coutinot@hotmail.com>
2022-07-06 12:51:48 +0200

    Project Blog

--- src/Entity/Article.php
+++ src/Entity/Article.php
@@ -2,61 +2,91 @@
 
 namespace App\Entity;
 
-
+use App\Repository\ArticleRepository;
 use Doctrine\ORM\Mapping as ORM;
 
-
-//on créer une classe (car route est pour les controllers) et on se sert de l'ORM
-// pour que l'annotation soit prise en compte et création d'une entité => class entité
-// ORM = mapping objet-relationnel ; outil qui va permettre de se mettre
-// en interface entre le programme et la BDD
 /**
- * @ORM\Entity()
+ * @ORM\Entity(repositoryClass=ArticleRepository::class)
  */
-
-//on créer une classe
-
-class Article {
-
-    //on créer les annotations pour definir ce qu'il va etre créé dans la BDD
-
+class Article
+{
     /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
      * @ORM\Column(type="integer")
-     * @ORM\Id()
-     * @ORM\GeneratedValue()
      */
-    public $id;
+    private $id;
 
     /**
-     * @ORM\Column (type="string")
+     * @ORM\Column(type="string", length=255)
      */
-    public $title;
+    private $title;
 
     /**
-     * @ORM\Column (type="string")
+     * @ORM\Column(type="boolean")
      */
-    public $image;
+    private $isPublished;
 
     /**
-     * @ORM\Column (type="date")
+     * @ORM\Column(type="string", length=255)
      */
-    public $isPublished;
+    private $author;
 
     /**
-     * @ORM\Column (type="string")
+     * @ORM\Column(type="string", length=255)
      */
-    public $author;
+    private $content;
 
-    // type string = chaine de caractere
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
 
-    // Pour creer ta base de donnée dans le terminal
-    // php bin/console doctrine:database:create
-    // migration de fichier dans la BDD
-    // php bin/console make:migration
-    // l'envoyer et le comparer de la BDD précédente pour le prendre en compte
-    // php bin/console doctrine:migration:migrate
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
 
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
 
-    //bin\console make:entity
-    //pour creer ta nvlle table directement
+        return $this;
+    }
+
+    public function isIsPublished(): ?bool
+    {
+        return $this->isPublished;
+    }
+
+    public function setIsPublished(bool $isPublished): self
+    {
+        $this->isPublished = $isPublished;
+
+        return $this;
+    }
+
+    public function getAuthor(): ?string
+    {
+        return $this->author;
+    }
+
+    public function setAuthor(string $author): self
+    {
+        $this->author = $author;
+
+        return $this;
+    }
+
+    public function getContent(): ?string
+    {
+        return $this->content;
+    }
+
+    public function setContent(string $content): self
+    {
+        $this->content = $content;
+
+        return $this;
+    }
 }
