1d1c94945f2adc8607b355e36203c23ae93364e3
ChaCoutinot <charlotte.coutinot@hotmail.com>
2022-07-12 13:48:00 +0200

    article Type

--- src/Form/ArticleType.php
+++ src/Form/ArticleType.php
@@ -3,6 +3,8 @@
 namespace App\Form;
 
 use App\Entity\Article;
+use App\Entity\Category;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -15,9 +17,15 @@ class ArticleType extends AbstractType
         $builder
             ->add('title')
             ->add('isPublished')
-            ->add('author')
             ->add('content')
-            ->add('isPublished')
+            // ajouter le champs category pour gerer la selection d'une categorie pour l'article
+                // mettre le type "EntityType" car relation vers une entité
+                // parametrer mon input pour qu'il affiche toutes les categories
+                // de la BDD avec leur titre dans les options du select 
+            ->add('category', EntityType::class, [
+                'class' => Category::class,
+                'choice_label'=> 'title'
+            ])
             ->add('submit', SubmitType::class)
         ;
     }
